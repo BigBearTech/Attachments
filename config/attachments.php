9ad7c9c8b3b914a3f8be10f141b6a55097e9cd80
Christopher <git@idragonmail.com>
2016-06-25 11:37:34 -0500

    Added the config

---
+++ config/attachments.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'table' => 'vendor_bigbeartech_attachments',
+    'post_attachment_route' => 'bigbeartech/attachment',
+    'middleware' => [],
+    'glide' => [
+        'on' => true,
+        'route' => 'bigbeartech/img/',
+    ]
+];
