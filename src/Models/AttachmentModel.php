4a453343e6f5023857d4a6c567a2c7a1e9497003
Christopher <git@idragonmail.com>
2016-06-25 11:40:14 -0500

    Added the model to save the attachments

---
+++ src/Models/AttachmentModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BigBearTech\Attachments\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttachmentModel extends Model
+{
+    protected $table = 'bigbeartech_attachments';
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'exif' => 'array',
+    ];
+}
